a96ccee8bc0f3c6d8a5518a9eded9f630ee528a9
Vaughn <vaughng1213@gmail.com>
2024-05-10 00:25:24 -0700

    example project! it really works!

---
+++ example.swim/products.py
@@ -0,0 +1,3 @@
+# This is a hypothetical embedable script. Users may want to fetch data via API
+# and parse it to a DataFrame so Swimport can use it. Users can also use
+# scripts to parse local non-csv files to a DataFrame.
