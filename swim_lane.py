77e8632603bc5c1209fbdb06293443426a574247
Vaughn <vaughng1213@gmail.com>
2024-05-11 01:33:15 -0700

    templating flow system back-end

--- swim_lane.py
+++ swim_lane.py
@@ -1,4 +1,5 @@
-import swim_operations
+import importlib.util
+import sys
 import pandas as pd
 import xml.etree.ElementTree as ET
 import tomllib
@@ -18,25 +19,34 @@ class Lane:
         lap = {'lane':self.lane_no,'operation':operation}
         self.orchestration.append(lap)
 
-
-def swim(project_name:str) -> pd.DataFrame:
-    'Now with custom properties!'
-    tree = ET.parse(f'''{project_name}/lanes.xml''')
-    swimlanes = tree.getroot().findall('swimlane')
-    for swimlane in swimlanes:
-        data = pd.read_csv(f'''{project_name}/{swimlane.find('input').text}''')
-        data.attrs['lane'] = swimlane.find('lane').text
-        for swimlap in swimlane.findall('swimlap'):
-            data = swim_operations.operate(data, swimlap)
-        return data
-
 # The only data type supported right now is tabular data as pd.DataFrame
 # TODO: Figure out the best way to load in non-tabular data - see products.py for an idea
-def swimx(project_name:str):
+def swim(project_name:str):
     tree = ET.parse(f'''{project_name}/lanes.xml''')
     swimlanes = tree.getroot().findall('swimlane')
+    data = {}
     for swimlane in swimlanes:
-        data = pd.read_csv(f'''{project_name}/{swimlane.find('input').text}''')
-        for swimlap in swimlane.findall('swimlap'):
-            data = swim_operations.operate(data, swimlap)
+        swimlane_name = swimlane.find('name').text
+
+        match swimlane.attrib['type']:
+            case 'pandas':
+                swimlaps = swimlane.find('swimlaps')
+                for lap in swimlaps:
+                    exec(lap.text, globals())
+                data.update({swimlane_name:globals()[swimlane_name]})
+            case 'script':
+                args = {}
+                for argument in swimlane.find('arguments').findall('argument'):
+                    argname = argument.find('argname').text
+                    argvalue = argument.find('argvalue').text
+                    args.update({argname:argvalue})
+                arguments = ','.join([f'{key}={value}' for key, value in args.items()])
+                ###
+                exec(f'spec = importlib.util.spec_from_file_location("{swimlane_name}_module", "{project_name}/scripts/{swimlane_name}.py")', globals())
+                exec(f'foo = importlib.util.module_from_spec(spec)', globals())
+                # sys.modules[swimlane_name] = foo
+                exec('spec.loader.exec_module(foo)', globals())
+                exec(f'{swimlane_name} = foo.main({arguments})', globals())
+        data.update({swimlane_name:globals()[swimlane_name]})
     return data
+                
