8fe78d377be5bdf73d708a7dac5336f50c5797ab
Vaughn <vaughng1213@gmail.com>
2024-05-13 00:17:33 -0700

    Preview XML contents poorly

--- swimview.py
+++ swimview.py
@@ -6,9 +6,16 @@ def lanespage(project_name:str):
     ui.link('Back','/')
     ui.label(f'{project_name}')
     lanes_xml = ET.parse(f'{project_name}/lanes.xml').getroot()
-    with ui.row():
-        for swimlane in lanes_xml:
+    for swimlane in lanes_xml:
+        with ui.row():
             ui.label(swimlane.find('name').text)
+            with ui.scroll_area().classes('w-64 h-32 border'):
+                lapview(swimlane)
 
-def laneview():
-    ...
+def lapview(swimlane):
+    match swimlane.get('type'):
+        case 'pandas':
+            for lap in swimlane.find('swimlaps').findall('operation'):
+                ui.label(lap.text)
+        case 'script ':
+            ui.label(swimlane.find('name').text)
